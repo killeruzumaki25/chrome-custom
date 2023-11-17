3a9c2313461a847237a5e7e30c79e232be88db89
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-17 11:27:40 -0800

    Update styleScript.js

--- styleScript.js
+++ styleScript.js
@@ -1,3 +1,31 @@
-body{
-  background-color:red;
+body{ 
+  scroll-behavior: smooth;
+}
+*{
+  z-index:9999;
+}
+.con{
+  background-color:grey;
+  width:35%;
+  height:50vh;
+  position:fixed;
+  left:65%;
+  display:flex;
+  z-index:9999;
+  top:0;
+  border-radius:2%;
+  overflow:hidden;
+  color:black;
+}
+.todoInput{
+  width:100%;
+  height:5vh;
+  outline:none;
+  border:1px solid black;
+}
+
+.saveBtn{
+  height:5vh;
+  width:20%;
+  border:1px solid black;
 }
