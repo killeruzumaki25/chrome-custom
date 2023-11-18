fb5d5f273d103c5adbccdc28b1f97471fd660912
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-17 16:26:05 -0800

    Update styleScript.js

--- styleScript.js
+++ styleScript.js
@@ -2,26 +2,44 @@ body{
   scroll-behavior: smooth;
 }
 *{
+  outline:0px solid red;
   z-index:9999;
 }
 .con{
-  background-color:grey;
-  width:35%;
-  height:50vh;
-  position:fixed;
-  left:65%;
-  display:flex;
-  z-index:9999;
-  top:0;
-  border-radius:2%;
-  overflow:hidden;
-  color:black;
-  flex-direction:row;
-  justify-content:space-around;
-  align-items:center;
-  flex-grow:1;
+  background-color: grey;
+  width: 320px; /* changed width */
+  height: 50vh;
+  position: fixed;
+  left: 65%;
+  display: flex;
+  z-index: 9999;
+  top: 0;
+  border-radius: 2%;
+  overflow: hidden;
+  color: black;
+  flex-direction: row;
 }
-.con button{
-  width:20%;
-  height:10vh;
+
+
+.mainCon{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  align-items:center;
+  justify-content:center;
+  margin-left:5%;
+}
+
+.closeBtn{
+  position:absolute;
+  width:10%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  left:90%;
+}
+
+.mainCon button{
+  width: 80px;
+  height: 80px;
 }
