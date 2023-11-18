376c569f1846f676e201dfec11f8c81393485a55
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-17 17:01:18 -0800

    Update styleScript.js

--- styleScript.js
+++ styleScript.js
@@ -1,13 +1,19 @@
+
+  @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Bebas+Neue&family=Black+Ops+One&family=Fuggles&family=Martian+Mono:wght@200&family=Noto+Sans&family=Sometype+Mono&display=swap');
+
 body{ 
   scroll-behavior: smooth;
 }
 *{
   outline:0px solid red;
   z-index:9999;
+  font-family: 'Black Ops One';
+  color:orange;
 }
 .con{
-  background-color: grey;
-  width: 320px; /* changed width */
+  background:rgba(0, 0, 255, 0.1);
+  border:2px solid blue;
+  width: 30%; /* changed width */
   height: 50vh;
   position: fixed;
   left: 65%;
@@ -28,6 +34,7 @@ body{
   align-items:center;
   justify-content:center;
   margin-left:5%;
+  margin-top:10%;
 }
 
 .closeBtn{
@@ -42,4 +49,36 @@ body{
 .mainCon button{
   width: 80px;
   height: 80px;
+}
+.conUp{
+  animation:conUp 0.3s linear;
+animation-fill-mode:both;
+}
+@keyframes conUp{
+100%{height:6vh}
+}
+
+.wallBtn{
+ background:url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwcvQsM8-g8UhqIkYhqovspXo29w2py-MZLaIBlHXL9w6X15KhrwmQrEplNiMeVNnrkZsYznXjYFlLXwJ0a-cEI5OQv7_MUv1MF5xc434aM88hZZ-JnU-Id8EGWHOcAXCBAkEZL3VKjCWE-BtfLP7p8UoO50SZ0rOGd2OtV1gz2hDOfxd3UP6AG3TViQ/w640-h360/4k-pc-wallpaper-ai-art.webp') ;
+  background-size:cover;
+}
+
+.colorTheme{
+  background:linear-gradient(90deg,black,white 50%)
+}
+.gadgetBtn{
+  background:url('https://cdn-icons-png.flaticon.com/512/1974/1974769.png');
+  background-size:cover;
+}
+.adRemove{
+  background:url('https://static.vecteezy.com/system/resources/previews/009/636/794/original/no-ads-icons-3d-illustration-icon-png.png');
+  background-size:cover;
+}
+.inspectBtn{
+  background:url('https://static.thenounproject.com/png/4199164-200.png');
+  background-size:cover;
+}
+.projectBtn{
+  background:url('https://pngimg.com/d/github_PNG19.png');
+  background-size:cover;
 }
