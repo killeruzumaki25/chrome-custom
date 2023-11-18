22593202fe218681fb44c87705075bd856ff7c78
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-17 17:52:17 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -1,115 +1,111 @@
+let oldBody = document.querySelector("body");
 
-  @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Bebas+Neue&family=Black+Ops+One&family=Fuggles&family=Martian+Mono:wght@200&family=Noto+Sans&family=Sometype+Mono&display=swap');
+let newHead = document.createElement('head');
+oldBody.appendChild(newHead);
 
-body{ 
-  scroll-behavior: smooth;
-}
-*{
-  outline:0px solid red;
-}
-.mainCon > * {
-  color: orange; /* replace "orange" with your desired color */
-  font-family: 'Black Ops One';
-  outline:0px solid red;
-  z-index:9999;
-}
+let icons = document.createElement('link');
+icons.innerHTML="<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0' />";
+newHead.appendChild(icons);
 
-.con >* {
-  color: orange; /* replace "orange" with your desired color */
-  font-family: 'Black Ops One';
-  outline:0px solid red;
-  z-index:9999;
-}
+const con = document.createElement('div');
+con.classList.add('con');
+oldBody.appendChild(con);
 
-.topCon{
-  width:200px;
-  background-color:white;
-  position:absolute;
-  height:6vh;
-}
-.backBtn{
-  border:none;
-}
+//span
 
-.con{
-  background:rgba(0, 0, 255, 0.1);
-  border:2px solid blue;
-  width: 30%; /* changed width */
-  height: 50vh;
-  position: fixed;
-  left: 65%;
-  display: flex;
-  z-index: 9999;
-  top: 0;
-  border-radius: 2%;
-  overflow: hidden;
-  color: black;
-  flex-direction: row;
-}
+let closeBtn = document.createElement("button");
+closeBtn.classList.add("closeBtn");
+closeBtn.innerHTML='<span class="material-symbols-outlined">arrow_upward</span>'
+con.appendChild(closeBtn);
+
+closeBtn.addEventListener('click',()=>{
+  con.classList.toggle('conUp')
+})
+
+const topCon = document.createElement('div');
+topCon.classList.add('topCon');
+con.appendChild(topCon);
+
+//titles
+let wallTitle = document.createElement('h2');
+wallTitle.innerText="Wallpaper";
+
+let colorThemeTitle = document.createElement('h2');
+colorThemeTitle.innerText="Themes";
+
+let gadgetTitle = document.createElement('h2');
+gadgetTitle.innerText="Gadget";
+
+let adRemoveTitle = document.createElement('h2');
+adRemoveTitle.innerText="Anti-ads";
+
+let inspectTitle = document.createElement('h2');
+inspectTitle.innerText="Inspect";
+
+let projectTitle = document.createElement('h2');
+projectTitle.innerText="project";
+
+let backBtn = document.createElement("button");
+backBtn.classList.add("backBtn");
+backBtn.innerHTML='<span class="material-symbols-outlined">arrow_back</span>';
+topCon.appendChild(backBtn);
+
+backBtn.addEventListener('click',()=>{
+  if(topCon.contains(wallTitle)){
+    wallTitle.remove();
+    con.appendChild(mainCon)
+  }else if(topCon.contains(colorThemeTitle)){
+    colorThemeTitle.remove();
+    con.appendChild(mainCon)
+  }
+})
+
+const mainCon = document.createElement('div');
+mainCon.classList.add('mainCon');
+con.appendChild(mainCon);
+
+let wallBtn = document.createElement('button');
+wallBtn.innerText="Wallpaper";
+wallBtn.classList.add('wallBtn');
+mainCon.appendChild(wallBtn);
+
+let wallDis = document.createElement('div');
+wallDis.classList.add('wallDis');
+
+wallBtn.addEventListener('click',()=>{
+  mainCon.remove();
+  topCon.appendChild(wallTitle);
+  con.appendChild(wallDis)
+})
 
 
-.mainCon{
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 10px;
-  align-items:center;
-  justify-content:center;
-  margin-left:5%;
-  margin-top:10%;
-  
-}
+let colorTheme = document.createElement("button");
+colorTheme.innerText="Theme"
+colorTheme.classList.add("colorTheme")
+mainCon.appendChild(colorTheme);
 
-.closeBtn{
-  position:absolute;
-  width:10%;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  left:90%;
-}
+colorTheme.addEventListener('click',()=>{
+  mainCon.remove();
+  topCon.appendChild(colorThemeTitle)
+})
 
-.mainCon button{
-  width: 80px;
-  height: 80px;
-}
-.conUp{
-  animation:conUp 0.3s linear;
-animation-fill-mode:both;
-}
-@keyframes conUp{
-100%{height:5vh}
-}
+let gadgetBtn= document.createElement("button");
+gadgetBtn.innerText="Gadget"
+gadgetBtn.classList.add("gadgetBtn")
+mainCon.appendChild(gadgetBtn);
 
-.wallBtn{
- background:url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwcvQsM8-g8UhqIkYhqovspXo29w2py-MZLaIBlHXL9w6X15KhrwmQrEplNiMeVNnrkZsYznXjYFlLXwJ0a-cEI5OQv7_MUv1MF5xc434aM88hZZ-JnU-Id8EGWHOcAXCBAkEZL3VKjCWE-BtfLP7p8UoO50SZ0rOGd2OtV1gz2hDOfxd3UP6AG3TViQ/w640-h360/4k-pc-wallpaper-ai-art.webp') ;
-  background-size:cover;
-}
+let adRemove = document.createElement("button");
+adRemove.innerText="Anti-ads"
+adRemove.classList.add("adRemove")
+mainCon.appendChild(adRemove);
 
-.colorTheme{
-  background:linear-gradient(90deg,black,white 50%)
-}
-.gadgetBtn{
-  background:url('https://cdn-icons-png.flaticon.com/512/1974/1974769.png');
-  background-size:cover;
-}
-.adRemove{
-  background:url('https://static.vecteezy.com/system/resources/previews/009/636/794/original/no-ads-icons-3d-illustration-icon-png.png');
-  background-size:cover;
-}
-.inspectBtn{
-  background:url('https://static.thenounproject.com/png/4199164-200.png');
-  background-size:cover;
-}
-.projectBtn{
-  background:url('https://pngimg.com/d/github_PNG19.png');
-  background-size:cover;
-}
-.topCon h2{
-  position:absolute;
-  top:-3vh;
-  margin-left:22%;
-}
+let inspectBtn = document.createElement("button");
+inspectBtn.innerText="Inspect"
+inspectBtn.classList.add("inspectBtn")
+mainCon.appendChild(inspectBtn);
 
-.wallDis{
-  
-}
+let projectBtn = document.createElement("button");
+projectBtn.innerText="Project"
+projectBtn.classList.add("projectBtn")
+mainCon.appendChild(projectBtn);
+
