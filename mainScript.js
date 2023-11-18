5127ce7e9325d27496bbbd96b9c3a373827d50de
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-17 16:29:16 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -11,22 +11,44 @@ const con = document.createElement('div');
 con.classList.add('con');
 oldBody.appendChild(con);
 
+//span
+
+let closeBtn = document.createElement("button");
+closeBtn.classList.add("closeBtn");
+closeBtn.innerHTML='<span class="material-symbols-outlined">arrow_upward</span>'
+con.appendChild(closeBtn);
+
+const mainCon = document.createElement('div');
+mainCon.classList.add('mainCon');
+con.appendChild(mainCon);
+
 let wallBtn = document.createElement('button');
 wallBtn.innerText="Wallpaper";
 wallBtn.classList.add('wallBtn');
-con.appendChild(wallBtn);
+mainCon.appendChild(wallBtn);
 
 let colorTheme = document.createElement("button");
 colorTheme.innerText="Theme"
 colorTheme.classList.add("colorTheme")
-con.appendChild(colorTheme);
+mainCon.appendChild(colorTheme);
 
 let gadgetBtn= document.createElement("button");
 gadgetBtn.innerText="Gadget"
 gadgetBtn.classList.add("colorTheme")
-con.appendChild(gadgetBtn);
+mainCon.appendChild(gadgetBtn);
 
 let adRemove = document.createElement("button");
-adRemove.innerText="Add remove"
+adRemove.innerText="Anti-ads"
 adRemove.classList.add("colorTheme")
-con.appendChild(adRemove);
+mainCon.appendChild(adRemove);
+
+let inspectBtn = document.createElement("button");
+inspectBtn.innerText="Inspect"
+inspectBtn.classList.add("inspectBtn")
+mainCon.appendChild(inspectBtn);
+
+let projectBtn = document.createElement("button");
+projectBtn.innerText="Project"
+projectBtn.classList.add("projectBtn")
+mainCon.appendChild(projectBtn);
+
