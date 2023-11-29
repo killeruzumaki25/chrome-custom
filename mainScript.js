6b2ba908c786833dff94345a1e4795234e051c99
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-29 09:22:47 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -127,7 +127,7 @@ aiBtn.addEventListener('click',()=>{
   con.appendChild(aiCon)
   topCon.appendChild(aiTitle)
   aiFrame.src="https://deepai.org/chat";
-  aiFrame.setAttribute("sandbox","");
+  aiFrame.setAttribute("sandbox","allow-same-origin allow-scripts");
   aiCon.appendChild(aiFrame)
 })
 
